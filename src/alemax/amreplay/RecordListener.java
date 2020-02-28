67097f215dd1ef08923fccc6ee2df658b84bccb0
AleMax <alemaxsng@gmail.com>
2020-02-28 12:43:55 +0100

    just a fun sound feature

--- src/alemax/amreplay/RecordListener.java
+++ src/alemax/amreplay/RecordListener.java
@@ -2,6 +2,7 @@ package alemax.amreplay;
 
 import alemax.amreplay.actions.Action;
 import alemax.amreplay.actions.BlockBreakAction;
+import org.bukkit.Sound;
 import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -13,10 +14,12 @@ public class RecordListener implements Listener {
 
     ArrayList<Action> actions;
     long startupTime;
+    public boolean sound;
 
     public RecordListener() {
         actions = new ArrayList<Action>();
         startupTime = System.currentTimeMillis();
+        sound = true;
     }
 
     @EventHandler
@@ -24,6 +27,9 @@ public class RecordListener implements Listener {
         Block block = event.getBlock();
         BlockBreakAction action = new BlockBreakAction(startupTime, block.getX(), block.getY(), block.getZ());
         actions.add(action);
+        if(sound) {
+            event.getPlayer().playSound(event.getPlayer().getLocation(), Sound.BLOCK_NOTE_BLOCK_BANJO, 1, 1);
+        }
     }
 
     public ArrayList<Action> getActions() {
--- src/alemax/amreplay/actions/ReplayCommand.java
+++ src/alemax/amreplay/actions/ReplayCommand.java
@@ -22,12 +22,34 @@ public class ReplayCommand implements CommandExecutor {
                 sender.sendMessage(ChatColor.GOLD + "" + ChatColor.BOLD + "\"/replay\"" + ChatColor.RESET + " " + ChatColor.GOLD + "Usage:\n"
                     + ChatColor.WHITE + "/replay " + ChatColor.GREEN + "setname " + ChatColor.RED + "<name>\n"
                     + ChatColor.WHITE + "/replay " + ChatColor.GREEN + "list\n"
-                    + ChatColor.WHITE + "/replay " + ChatColor.GREEN + "currentActions"
+                    + ChatColor.WHITE + "/replay " + ChatColor.GREEN + "currentActions\n"
+                    + ChatColor.WHITE + "/replay " + ChatColor.GREEN + "sounds " + ChatColor.RED + "<on/off>"
                         );
                 return true;
             } else if(args[0].equals("currentActions")) {
                 for(Action action : recordListener.getActions()) {
                     sender.sendMessage(action.toString());
+                }
+                return true;
+            } else if(args[0].equals("sounds")) {
+                if(args.length > 1) {
+                    if(args[1].equals("off")) {
+                        if(recordListener.sound) {
+                            recordListener.sound = false;
+                            sender.sendMessage(ChatColor.RED + "Disabled Sounds!");
+                        } else {
+                            sender.sendMessage(ChatColor.GOLD + "Sounds already disabled");
+                        }
+                        return true;
+                    } else if(args[1].equals("on")) {
+                        if(!recordListener.sound) {
+                            recordListener.sound = true;
+                            sender.sendMessage(ChatColor.GREEN + "Enabled Sounds!");
+                        } else {
+                            sender.sendMessage(ChatColor.GOLD + "Sounds already enabled");
+                        }
+                        return true;
+                    }
                 }
             }
 
