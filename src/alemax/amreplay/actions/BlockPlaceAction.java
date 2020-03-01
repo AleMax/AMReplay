83ddf238445179d3d8f388b373f725980f227ea2
AleMax <alemaxsng@gmail.com>
2020-03-01 17:48:03 +0100

    Some more block cases

--- src/alemax/amreplay/actions/BlockPlaceAction.java
+++ src/alemax/amreplay/actions/BlockPlaceAction.java
@@ -6,9 +6,9 @@ import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
-import org.bukkit.block.data.BlockData;
-import org.bukkit.block.data.Orientable;
-import org.bukkit.block.data.Rotatable;
+import org.bukkit.block.data.*;
+import org.bukkit.block.data.type.Slab;
+import org.bukkit.block.data.type.Stairs;
 import org.bukkit.event.block.BlockDamageEvent;
 
 import java.lang.reflect.Array;
@@ -35,6 +35,9 @@ public class BlockPlaceAction extends Action {
         /*
         0x01: Rotatable: 1 byte --> Lookup
         0x02: Orientable: 1 byte --> lookup
+        0x03: Directional: 1 byte --> lookup
+        0x04: Bisected: 1 byte --> lookup
+        0x05: Slab: 1 byte --> lookup
          */
         BlockData data = block.getBlockData();
         if(data instanceof Rotatable) {
@@ -71,6 +74,63 @@ public class BlockPlaceAction extends Action {
                 case X: additionalBytes.add((byte) 0x01); break;
                 case Y: additionalBytes.add((byte) 0x02); break;
                 case Z: additionalBytes.add((byte) 0x03); break;
+                default: additionalBytes.add((byte) 0x01); break;
+            }
+        }
+        if(data instanceof Directional) {
+            additionalBytes.add((byte) 0x03);
+            BlockFace blockFace = ((Directional) data).getFacing();
+            switch(blockFace) {
+                case NORTH: additionalBytes.add((byte) 0x01); break;
+                case EAST: additionalBytes.add((byte) 0x02); break;
+                case SOUTH: additionalBytes.add((byte) 0x03); break;
+                case WEST: additionalBytes.add((byte) 0x04); break;
+                case UP: additionalBytes.add((byte) 0x05); break;
+                case DOWN: additionalBytes.add((byte) 0x06); break;
+                case NORTH_EAST: additionalBytes.add((byte) 0x07); break;
+                case NORTH_WEST: additionalBytes.add((byte) 0x08);
+                case SOUTH_EAST: additionalBytes.add((byte) 0x09); break;
+                case SOUTH_WEST: additionalBytes.add((byte) 0x0A); break;
+                case WEST_NORTH_WEST: additionalBytes.add((byte) 0x0B); break;
+                case NORTH_NORTH_WEST: additionalBytes.add((byte) 0x0C); break;
+                case NORTH_NORTH_EAST: additionalBytes.add((byte) 0x0D); break;
+                case EAST_NORTH_EAST: additionalBytes.add((byte) 0x0E); break;
+                case EAST_SOUTH_EAST: additionalBytes.add((byte) 0x0F); break;
+                case SOUTH_SOUTH_EAST: additionalBytes.add((byte) 0x10); break;
+                case SOUTH_SOUTH_WEST: additionalBytes.add((byte) 0x11); break;
+                case WEST_SOUTH_WEST: additionalBytes.add((byte) 0x12); break;
+                case SELF: additionalBytes.add((byte) 0x13); break;
+                default: additionalBytes.add((byte) 0x01); break;
+            }
+        }
+        if(data instanceof Bisected) {
+            additionalBytes.add((byte) 0x04);
+            Bisected.Half half = ((Bisected) data).getHalf();
+            switch(half) {
+                case TOP: additionalBytes.add((byte) 0x01); break;
+                case BOTTOM: additionalBytes.add((byte) 0x02); break;
+                default: additionalBytes.add((byte) 0x02); break;
+            }
+        }
+        if(data instanceof Slab) {
+            additionalBytes.add((byte) 0x05);
+            Slab.Type type = ((Slab) data).getType();
+            switch(type) {
+                case TOP: additionalBytes.add((byte) 0x01); break;
+                case BOTTOM: additionalBytes.add((byte) 0x02); break;
+                case DOUBLE: additionalBytes.add((byte) 0x03); break;
+                default: additionalBytes.add((byte) 0x02); break;
+            }
+        }
+        if(data instanceof Stairs) {
+            additionalBytes.add((byte) 0x06);
+            Stairs.Shape shape = ((Stairs) data).getShape();
+            switch(shape) {
+                case STRAIGHT: additionalBytes.add((byte) 0x01); break;
+                case INNER_LEFT: additionalBytes.add((byte) 0x02); break;
+                case INNER_RIGHT: additionalBytes.add((byte) 0x03); break;
+                case OUTER_LEFT: additionalBytes.add((byte) 0x04); break;
+                case OUTER_RIGHT: additionalBytes.add((byte) 0x05); break;
                 default: additionalBytes.add((byte) 0x01); break;
             }
         }
@@ -123,6 +183,55 @@ public class BlockPlaceAction extends Action {
                         case 0x02: ((Orientable) data).setAxis(Axis.Y); break;
                         case 0x03: ((Orientable) data).setAxis(Axis.Z); break;
                         default: ((Orientable) data).setAxis(Axis.X); break;
+                    }
+                    index += 2;
+                } else if(additionalBytes.get(index) == 3) {
+                    switch(additionalBytes.get(index + 1)) {
+                        case 0x01: ((Directional) data).setFacing(BlockFace.NORTH); break;
+                        case 0x02: ((Directional) data).setFacing(BlockFace.EAST); break;
+                        case 0x03: ((Directional) data).setFacing(BlockFace.SOUTH); break;
+                        case 0x04: ((Directional) data).setFacing(BlockFace.WEST); break;
+                        case 0x05: ((Directional) data).setFacing(BlockFace.UP); break;
+                        case 0x06: ((Directional) data).setFacing(BlockFace.DOWN); break;
+                        case 0x07: ((Directional) data).setFacing(BlockFace.NORTH_EAST); break;
+                        case 0x08: ((Directional) data).setFacing(BlockFace.NORTH_WEST); break;
+                        case 0x09: ((Directional) data).setFacing(BlockFace.SOUTH_EAST); break;
+                        case 0x0A: ((Directional) data).setFacing(BlockFace.SOUTH_WEST); break;
+                        case 0x0B: ((Directional) data).setFacing(BlockFace.WEST_NORTH_WEST); break;
+                        case 0x0C: ((Directional) data).setFacing(BlockFace.NORTH_NORTH_WEST); break;
+                        case 0x0D: ((Directional) data).setFacing(BlockFace.NORTH_NORTH_EAST); break;
+                        case 0x0E: ((Directional) data).setFacing(BlockFace.EAST_NORTH_EAST); break;
+                        case 0x0F: ((Directional) data).setFacing(BlockFace.EAST_SOUTH_EAST); break;
+                        case 0x10: ((Directional) data).setFacing(BlockFace.SOUTH_SOUTH_EAST); break;
+                        case 0x11: ((Directional) data).setFacing(BlockFace.SOUTH_SOUTH_WEST); break;
+                        case 0x12: ((Directional) data).setFacing(BlockFace.WEST_SOUTH_WEST); break;
+                        case 0x13: ((Directional) data).setFacing(BlockFace.SELF); break;
+                        default: ((Directional) data).setFacing(BlockFace.NORTH); break;
+                    }
+                    index += 2;
+                } else if(additionalBytes.get(index) == 4) {
+                    switch(additionalBytes.get(index + 1)) {
+                        case 0x01: ((Bisected) data).setHalf(Bisected.Half.TOP); break;
+                        case 0x02: ((Bisected) data).setHalf(Bisected.Half.BOTTOM); break;
+                        default: ((Bisected) data).setHalf(Bisected.Half.BOTTOM); break;
+                    }
+                    index += 2;
+                } else if(additionalBytes.get(index) == 5) {
+                    switch(additionalBytes.get(index + 1)) {
+                        case 0x01: ((Slab) data).setType(Slab.Type.TOP); break;
+                        case 0x02: ((Slab) data).setType(Slab.Type.BOTTOM); break;
+                        case 0x03: ((Slab) data).setType(Slab.Type.DOUBLE); break;
+                        default: ((Slab) data).setType(Slab.Type.BOTTOM); break;
+                    }
+                    index += 2;
+                } else if(additionalBytes.get(index) == 6) {
+                    switch(additionalBytes.get(index + 1)) {
+                        case 0x01: ((Stairs) data).setShape(Stairs.Shape.STRAIGHT); break;
+                        case 0x02: ((Stairs) data).setShape(Stairs.Shape.INNER_LEFT); break;
+                        case 0x03: ((Stairs) data).setShape(Stairs.Shape.INNER_RIGHT); break;
+                        case 0x04: ((Stairs) data).setShape(Stairs.Shape.OUTER_LEFT); break;
+                        case 0x05: ((Stairs) data).setShape(Stairs.Shape.OUTER_RIGHT); break;
+                        default: ((Stairs) data).setShape(Stairs.Shape.STRAIGHT); break;
                     }
                     index += 2;
                 }
